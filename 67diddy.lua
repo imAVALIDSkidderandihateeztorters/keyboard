1306a4d5cf22f1f93753f80d27a682ec237519f2
imAVALIDSkidderandihateeztorters <kadenburgoon1@gmail.com>
2025-11-09 19:05:46 -0600

    Create 67diddy.lua

---
+++ 67diddy.lua
@@ -0,0 +1,19 @@
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local placeidthing = 136253801690395
+
+if not player then
+    return
+end
+
+local smallpenis, diddyblud = pcall(function()
+    TeleportService:Teleport(placeidthing, player)
+end)
+
+if not smallpenis then
+    print('penis')
+else
+    print('bigger penis')
+end
