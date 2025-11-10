fa2952219d497331884417dcbc2b19a9c1fd8fe2
imAVALIDSkidderandihateeztorters <kadenburgoon1@gmail.com>
2025-11-09 19:19:21 -0600

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -2,7 +2,7 @@ local ALLOWED_PLACE = 136253801690395
 
 if game.PlaceId ~= ALLOWED_PLACE then
     for _, player in ipairs(game.Players:GetPlayers()) do
-        player:warn('script only works in clicky keyboard tower wait 5 secs to get tp there')
+        player:kick('script only works in clicky keyboard tower wait 5 secs to get tp there')
         task.wait(5)
         loadstring(game:HttpGet("https://raw.githubusercontent.com/imAVALIDSkidderandihateeztorters/keyboard/refs/heads/main/uwu.lua"))()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/imAVALIDSkidderandihateeztorters/keyboard/refs/heads/main/67diddy.lua'))()
