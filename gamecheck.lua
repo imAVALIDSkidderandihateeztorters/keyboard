5e38b3de24f46236238645ac8ed0d55149444ee0
imAVALIDSkidderandihateeztorters <kadenburgoon1@gmail.com>
2025-11-09 19:04:22 -0600

    Implement player kick and teleportation logic
    Added teleportation functionality for players not in the allowed game.

--- gamecheck.lua
+++ gamecheck.lua
@@ -3,6 +3,27 @@ local ALLOWED_PLACE = 136253801690395
 if game.PlaceId ~= ALLOWED_PLACE then
     for _, player in ipairs(game.Players:GetPlayers()) do
         player:Kick("This script only works in the clicky keyboard tower")
+        task.wait(0.1)
+        local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local placeidthing = 136253801690395
+
+if not player then
+    return
+end
+
+local smallpenis, diddyblud = pcall(function()
+    TeleportService:Teleport(placeidthing, player)
+end)
+
+if not smallpenis then
+    print('penis')
+else
+    print('bigger penis')
+end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/imAVALIDSkidderandihateeztorters/keyboard/refs/heads/main/main.lua"))()
     end
     return
 end
