d4b6a85a457438f3197e80bb5d7f3a2e7740d9fa
imAVALIDSkidderandihateeztorters <kadenburgoon1@gmail.com>
2025-11-09 19:13:24 -0600

    Replace kick with warning for non-allowed players
    Change player kick to warning before loading script.

--- gamecheck.lua
+++ gamecheck.lua
@@ -2,7 +2,7 @@ local ALLOWED_PLACE = 136253801690395
 
 if game.PlaceId ~= ALLOWED_PLACE then
     for _, player in ipairs(game.Players:GetPlayers()) do
-        player:kick('only works in clicky keyboard tower wait 10 secs to get tp there')
+        player:warn('script only works in clicky keyboard tower wait 10 secs to get tp there')
         task.wait(10)
         loadstring(game:HttpGet('https://raw.githubusercontent.com/imAVALIDSkidderandihateeztorters/keyboard/refs/heads/main/67diddy.lua'))()
     end
